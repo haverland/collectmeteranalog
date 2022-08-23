edb0fa538a681a3981e4bb57881cb538f9a1474e
Frank Haverland <fspapaping@googlemail.com>
2022-08-23 20:32:22 +0200

    try exe only

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = """
 
 setup(
     name='collectmeteranalog',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/haverland/collectmeteranalog',
     license='Apache 2.0',
     author='Frank Haverland',
