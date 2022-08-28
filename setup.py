33e0746e481adc84b813931e56a46ba6882980c2
Frank Haverland <fspapaping@googlemail.com>
2022-08-28 22:52:47 +0200

    raise version to 0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = """
 
 setup(
     name='collectmeteranalog',
-    version='0.1.1',
+    version='0.2.0',
     url='https://github.com/haverland/collectmeteranalog',
     license='Apache 2.0',
     author='Frank Haverland',
